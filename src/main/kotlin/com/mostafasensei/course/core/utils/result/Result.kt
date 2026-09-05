bf2406ac4a23d1ee7872e61a61223e8adf1182c4
MostafaSensei106 <wwwmostafagmer100'gmail.com>
2026-09-05 15:07:55 +0300

    Refactor Result class: rename Success value to data and improve data/error handling

--- src/main/kotlin/com/mostafasensei/course/core/utils/result/Result.kt
+++ src/main/kotlin/com/mostafasensei/course/core/utils/result/Result.kt
@@ -1,7 +1,7 @@
 package com.mostafasensei.course.core.utils.result
 
 sealed interface Result<out T, out E> {
-    data class Success<T>(val value: T) : Result<T, Nothing>
+    data class Success<T>(val data: T) : Result<T, Nothing>
     data class Failure<E>(val error: E) : Result<Nothing, E>
 
 
@@ -31,13 +31,41 @@ sealed interface Result<out T, out E> {
             get() = this is Failure<E>
 
         val <T, E> Result<T, E>.dataOrNull: T? get() = when (this) {
-            is Result.Success -> this.value
-            is Result.Failure -> null
+            is Success -> data
+            is Failure -> null
         }
 
-        val <T, E> Result<T,E>.errorOrNull: E? get() = when (this) {is Result.Success -> null
-        is Result.Failure -> this.error
+        val <T, E> Result<T,E>.errorOrNull: E? get() = when (this) {is Success -> null
+        is Failure -> error
         }
+
+        inline fun <T,E,R> Result<T,E>.fold(
+            onSuccess: (data: T) -> R,
+            onFailure: (error: E) -> R
+        ): R = when (this) {
+            is Success -> onSuccess(data)
+            is Failure -> onFailure(error)
+        }
+
+        inline fun <T, E> Result<T, E>.onFailure(action: (error: E) -> Unit): Result<T, E> {
+            if (this is Failure) action(error)
+            return this
+        }
+
+
+        inline fun <T, E, NewError> Result<T, E>.mapError(mapper: (error: E) -> NewError): Result<T, NewError> =
+            when (this) {
+                is Success -> Success(data)
+                is Failure -> Failure(mapper(error))
+            }
+
+        inline fun <T, E, NewData> Result<T, E>.map(mapper: (data: T) -> NewData): Result<NewData, E> =
+            when (this) {
+                is Success -> Success(mapper(data))
+                is Failure -> Failure(error)
+            }
+
+
     }
 
 }
